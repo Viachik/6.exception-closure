1cf6060b304fc91080c27e2a18203b7b7cca6f14
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-12-04 23:43:00 +0300

    after check 3

--- task.js
+++ task.js
@@ -24,6 +24,38 @@ class Triangle {
       if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
           throw new Error("Треугольник с такими сторонами не существует");
       }
+      this.side1 = side1;
+      this.side2 = side2;
+      this.side3 = side3;
+
+      let getPerimeter = side1 + side2 + side3;
+      let getArea = Number(Math.sqrt((getPerimeter/2)*(getPerimeter/2 - side1)*(getPerimeter/2 - side2)*(getPerimeter/2 - side3)).toFixed(2));
+       return [getPerimeter, getArea]
+  }
+}
+
+let triangle = new Triangle(1, 2, 3);
+console.log(triangle)
+
+function getTriangle(side1, side2, side3){
+    try {
+      return new Triangle(side1, side2, side3);
+    } catch {
+        let exception = {};
+        exception.getArea = "Ошибка! Треугольник не существует";
+        exception.getPerimeter = "Ошибка! Треугольник не существует";
+        return exception;
+    }
+}
+
+/*
+Задача №2. Треугольник
+
+class Triangle {
+  constructor(side1, side2, side3) {
+      if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
+          throw new Error("Треугольник с такими сторонами не существует");
+      }
       this.getPerimeter = getPerimeter;
       this.getArea = getArea;
   }
@@ -47,22 +79,5 @@ function getTriangle(side1, side2, side3){
         exception.getPerimeter = "Ошибка! Треугольник не существует";
         return exception;
     }
-}
-
-/*
-class Triangle {
-  constructor(side1, side2, side3, getPerimeter, getArea) {
-      if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
-          throw new Error("Треугольник с такими сторонами не существует");
-      }
-      this.getPerimeter = getPerimeter;
-      this.getArea = getArea;
-  }
-  getPerimeter(side1, side2, side3) {
-    return (side1 + side2 + side3);
-  }
-  getArea(getPerimeter, side1, side2, side3) {
-    return Math.sqrt((getPerimeter/2)*(getPerimeter/2 - side1)*(getPerimeter/2 - side2)*(getPerimeter/2 - side3)).toFixed(2);
-  }
 }
 */
