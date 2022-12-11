80d811a787db1feb5f08e5bab2768d4359e0962d
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-12-11 22:08:25 +0300

    after check 6

--- task.js
+++ task.js
@@ -20,20 +20,21 @@ function validateCount(element) {
 // Задача №2. Треугольник
 
 class Triangle {
-  constructor(side1, side2, side3) {
+  constructor(side1, side2, side3, perimeter) {
       if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
           throw new Error("Треугольник с такими сторонами не существует");
       }
       this.side1 = side1;
       this.side2 = side2;
       this.side3 = side3;
+      this.perimeter = perimeter;
   }
   getPerimeter() {
     return (this.side1 + this.side2 + this.side3);
   }
   getArea() {
-    let perimeter = this.side1 + this.side2 + this.side3;
-    return (Number(Math.sqrt((perimeter/2)*(perimeter/2 - this.side1)*(perimeter/2 - this.side2)*(perimeter/2 - this.side3)).toFixed(2)));
+    let halfPerimeter = getPerimeter()/2;
+    return (Number(Math.sqrt((halfPerimeter)*(halfPerimeter - this.side1)*(halfPerimeter - this.side2)*(halfPerimeter - this.side3)).toFixed(2)));
   }
 }
 
@@ -46,10 +47,10 @@ function getTriangle(side1, side2, side3){
     } catch {
         let exception = {};
         exception.getArea = () => {
-          console.log("Ошибка! Треугольник не существует");
+          return "Ошибка! Треугольник не существует";
         }
         exception.getPerimeter = () => {
-          console.log("Ошибка! Треугольник не существует");
+          return "Ошибка! Треугольник не существует";
         }
         return exception;
     }
