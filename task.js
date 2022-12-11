9eabe453db7e3b8d821bff0b18bcc76de5a42dbb
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-12-11 22:57:28 +0300

    after check 7

--- task.js
+++ task.js
@@ -20,20 +20,19 @@ function validateCount(element) {
 // Задача №2. Треугольник
 
 class Triangle {
-  constructor(side1, side2, side3, perimeter) {
+  constructor(side1, side2, side3) {
       if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
           throw new Error("Треугольник с такими сторонами не существует");
       }
       this.side1 = side1;
       this.side2 = side2;
       this.side3 = side3;
-      this.perimeter = perimeter;
   }
   getPerimeter() {
     return (this.side1 + this.side2 + this.side3);
   }
   getArea() {
-    let halfPerimeter = getPerimeter()/2;
+    let halfPerimeter = this.getPerimeter/2;
     return (Number(Math.sqrt((halfPerimeter)*(halfPerimeter - this.side1)*(halfPerimeter - this.side2)*(halfPerimeter - this.side3)).toFixed(2)));
   }
 }
