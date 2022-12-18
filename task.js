4c051e2c939efc9c89a322a1998c427bdff9193b
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-12-18 20:47:13 +0300

    after check 8

--- task.js
+++ task.js
@@ -21,6 +21,46 @@ function validateCount(element) {
 
 class Triangle {
   constructor(side1, side2, side3) {
+      if ((this.side1 + this.side2) < this.side3 || (this.side1 + this.side3) < this.side2 || (this.side2 + this.side3) < this.side1) {
+          throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.side1 = side1;
+      this.side2 = side2;
+      this.side3 = side3;
+  }
+  getPerimeter() {
+    return (this.side1 + this.side2 + this.side3);
+  }
+  getArea() {
+    let this.halfPerimeter = (this.side1 + this.side2 + this.side3)/2;
+    return (Number(Math.sqrt((this.halfPerimeter)*(this.halfPerimeter - this.side1)*(this.halfPerimeter - this.side2)*(this.halfPerimeter - this.side3)).toFixed(2)));
+  }
+}
+
+
+let triangle = new Triangle(1, 2, 3);
+console.log(triangle)
+
+function getTriangle(side1, side2, side3){
+    try {
+      return new Triangle(side1, side2, side3);
+    } catch {
+        let exception = {};
+        exception.getArea = () => {
+          return "Ошибка! Треугольник не существует";
+        }
+        exception.getPerimeter = () => {
+          return "Ошибка! Треугольник не существует";
+        }
+        return exception;
+    }
+}
+
+/*
+Задача №2. Треугольник
+
+class Triangle {
+  constructor(side1, side2, side3) {
       if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
           throw new Error("Треугольник с такими сторонами не существует");
       }
@@ -51,39 +91,6 @@ function getTriangle(side1, side2, side3){
         exception.getPerimeter = () => {
           return "Ошибка! Треугольник не существует";
         }
-        return exception;
-    }
-}
-
-/*
-Задача №2. Треугольник
-
-class Triangle {
-  constructor(side1, side2, side3) {
-      if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
-          throw new Error("Треугольник с такими сторонами не существует");
-      }
-      this.getPerimeter = getPerimeter;
-      this.getArea = getArea;
-  }
-  getPerimeter(side1, side2, side3) {
-    return (side1 + side2 + side3);
-  }
-  getArea(getPerimeter, side1, side2, side3) {
-    return Math.sqrt((getPerimeter/2)*(getPerimeter/2 - side1)*(getPerimeter/2 - side2)*(getPerimeter/2 - side3)).toFixed(2);
-  }
-}
-
-let triangle = new Triangle(1, 2, 3);
-console.log(triangle)
-
-function getTriangle(side1, side2, side3){
-    try {
-      return new Triangle(side1, side2, side3);
-    } catch {
-        let exception = {};
-        exception.getArea = "Ошибка! Треугольник не существует";
-        exception.getPerimeter = "Ошибка! Треугольник не существует";
         return exception;
     }
 }
